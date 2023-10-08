fe95cb9bc40b39fdad6573e0d73e345d6c5f1982
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2023-10-08 21:45:30 +0300

    Entity sınıfları eklendi

---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Contact
+    {
+        public int ContactID { get; set; }
+
+        public string ContactUserName { get; set; }
+
+        public string ContactMail { get; set; }
+
+        public string ContactSubject { get; set; }
+
+        public string ContactMessage { get; set; }
+
+        public DateTime ContatcDate { get; set; }
+
+        public bool  ContactStatus { get; set; }
+    }
+}
