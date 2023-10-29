b6962e9a796612817aa761bd490b9bd01b6774b0
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2023-10-29 17:44:11 +0300

    Entity Framework klasörü eklendi ve bağımlılıkları tanımlandı

---
+++ DataAccesLayer/EntityFramework/EfAboutRepository.cs
@@ -0,0 +1,10 @@
+﻿using DataAccesLayer.Abstract;
+using DataAccesLayer.Repositories;
+using EntityLayer.Concrete;
+
+namespace DataAccesLayer.EntityFramework
+{
+    public class EfAboutRepository : GenericRepository<About> , IAboutDal
+    {
+    }
+}
