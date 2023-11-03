a3cd4f08892f63dbde6a347279c7dc9b83592968
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2023-11-03 23:42:59 +0300

    03.11.2023

---
+++ BlogApplication/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApplication.ViewComponents.Comment
+{
+    public class CommentListByBlog : ViewComponent
+    {
+        CommentManager cm = new CommentManager(new EfCommentRepository());
+
+        public IViewComponentResult Invoke(int blogID)
+        {
+            var values = cm.GetList(blogID);
+            return View(values);
+        }
+    }
+}
