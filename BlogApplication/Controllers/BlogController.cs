26df4bcb976e3580ed22c7987538897c20a53a0e
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2023-11-24 00:16:29 +0300

    24.11.2023 -3

--- BlogApplication/Controllers/BlogController.cs
+++ BlogApplication/Controllers/BlogController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccesLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,9 +25,46 @@ namespace BlogApplication.Controllers
             return View(values);
         }
 
-		public IActionResult TestPage()
-		{
-			return View();
-		}
-	}
+        public IActionResult TestPage()
+        {
+            return View();
+        }
+
+        public IActionResult BlogListByWriter()
+        {
+            var values = bm.GetBlogListWitWriter(1);
+            return View(values);
+
+        }
+
+        [HttpGet]
+        public IActionResult BlogAdd()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult BlogAdd(Blog blog)
+        {
+            BlogValidator blogValidator = new BlogValidator();
+            ValidationResult results = blogValidator.Validate(blog);
+            if (results.IsValid)
+            {
+                blog.WriterID = 1;
+                blog.BlogStatus = true;
+                blog.BlogCreateDate = DateTime.Now;
+                bm.Add(blog);
+                return RedirectToAction("BlogListByWriter" , "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
 }
---
+++ BusinessLayer/ValidationRules/BlogValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class BlogValidator : AbstractValidator<Blog>
+    {
+        public BlogValidator()
+        {
+            //RuleFor(x => x.CommentUserName).NotEmpty().WithMessage("Lütfen kullanıcı adınızı giriniz");
+            //RuleFor(x => x.CommentContent).NotEmpty().WithMessage("Lütfen içeriğini doldurunuz");
+        }
+    }
+}
