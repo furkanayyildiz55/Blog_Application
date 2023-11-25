425eea0746795911805f06d5be6d8592bc5bd0c9
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2023-11-25 23:09:40 +0300

    25.11.2023

---
+++ BlogApplication/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApplication.ViewComponents.Blog
+{
+	public class BlogListDashboard : ViewComponent
+	{
+
+		BlogManager BlogManager = new BlogManager(new EfBlogRepository());
+
+
+		public IViewComponentResult Invoke()
+		{
+			var values = BlogManager.GetBlogListWithCategory();
+			return View(values);
+		}
+
+	}
+}
