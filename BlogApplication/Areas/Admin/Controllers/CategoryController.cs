275bc00ab6f62d77db2527bd9ce9a43206ad5ad5
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2023-12-02 18:31:07 +0300

    02.12.2023-Pagination

--- BlogApplication/Areas/Admin/Controllers/CategoryController.cs
+++ BlogApplication/Areas/Admin/Controllers/CategoryController.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
 
 namespace BlogApplication.Areas.Admin.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+
         [Area("Admin")]
-        public IActionResult Index()
+        public IActionResult Index(int page=1)
         {
-            return View();
+            var values = categoryManager.GetList().ToPagedList(page,3);
+            return View(values);
         }
     }
 }
